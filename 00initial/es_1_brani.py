eaa36d3d93b2f5e9223acf180607e10d860958ce
SamueleLibasci <samuelelibasci2000@gmail.com>
2026-09-24 12:54:19 +0200

    proriuuy

--- 00initial/es_1_brani.py
+++ 00initial/es_1_brani.py
@@ -1,1 +1,37 @@
-a = 5
+class Brano:
+    def __init__(self, titolo, autore, durata):
+        self._titolo = titolo
+        self._autore = autore
+        self._durata = durata
+
+    @property
+    def titolo(self):
+        return self._titolo
+
+    @titolo.setter
+    def titolo(self, valore):
+        self._titolo = valore
+
+    @property
+    def autore(self):
+        return self._autore
+
+    @autore.setter
+    def autore(self, valore):
+        self._autore = valore
+
+    @property
+    def durata(self):
+        return self._durata
+
+    @durata.setter
+    def durata(self, valore):
+        self._durata = valore
+
+    def __str__(self):
+        return f"{self.titolo} - {self.autore} ({self.durata} sec)"
+
+
+if __name__ == "__main__":
+    newBrano = Brano("Imagine", "John Lennon", 180)
+    print(newBrano)
